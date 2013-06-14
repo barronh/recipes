35d70c9534509223cb6a803b5e0accc6711bb923
barronh <barronh@gmail.com>
2013-06-14 00:07:39 -0300

    Adding pure IOAPI capabilities
    Needs a good test

--- AddLatLonToNetCDFCFLambertConformal.py
+++ AddLatLonToNetCDFCFLambertConformal.py
@@ -5,13 +5,13 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.set_usage("""
 
-Usage: python %prog ifile [lccname]
+Usage: python %prog [-m CF|IOAPI] ifile [lccname]
 
   ifile - path to NetCDF file with lccname variable
-  lccname - name of variable that defines the Lambert 
+  lccname - name of CF variable that defines the Lambert 
             Conical Conformal Projection. defaults to 
             LambertConformalProjection (e.g., CAMx or
-            CMAQ file converted to NetCDF using netcdf-java) 
+            CMAQ file converted to NetCDF using netcdf-java)
 """)
 
 
@@ -32,14 +32,29 @@ from mpl_toolkits.basemap import pyproj
 # Open inputfile (ifile should be some path to a file
 ifileo = Dataset(ifile, 'a')
 
-try:
-    lccdef = ifileo.variables[lccname]
-except KeyError as e:
-    raise KeyError(e.message + ' -- Available keys: ' + ', '.join(ifileo.variables.keys()))
+if 'CF' in options.conventions:
+    try:
+        lccdef = ifileo.variables[lccname]
+    except KeyError as e:
+        raise KeyError(e.message + ' -- Available keys: ' + ', '.join(ifileo.variables.keys()))
+    x, y = ifileo.variables['x'][:], ifileo.variables['y'][:]
+    lunit = ifileo.variables['x'].units.strip()
+elif 'IOAPI' in options.conventions:
+    lccdef = object()
+    lccdef.latitude_projection_origin = ifileo.YCENT
+    lccdef.standard_parallel = [ifileo.P_ALP, ifileo.P_BET]
+    lccdef.earth_radius = 6371000. # Add search for earth radius later
+    lccdef.longitude_of_central_meridian = ifileo.XCENT
+    x = np.linspace(0, ifileo.NCOLS * ifileo.XCELL, ifileo.NCOLS + 1) + ifileo.XORIG
+    y = np.linspace(0, ifileo.NROWS * ifileo.XCELL, ifileo.NROWS + 1) + ifileo.YORIG
+    lunit = 'm'
+
 lcc = pyproj.Proj('+proj=lcc +lon_0=%s +lat_1=%s +lat_2=%s +a=%s +lat_0=%s' % (lccdef.longitude_of_central_meridian, lccdef.standard_parallel[0], lccdef.standard_parallel[1], lccdef.earth_radius, lccdef.latitude_of_projection_origin,)  )
 
-scale = {'km': 1000., 'm': 1.}[ifileo.variables['x'].units.strip()]
-lcc_x, lcc_y = np.meshgrid(ifileo.variables['x'], ifileo.variables['y'])
+if 'IOAPI' in options.conventions:
+    x = arrange
+scale = {'km': 1000., 'm': 1.}[lunit]
+lcc_x, lcc_y = np.meshgrid(x, y)
 lcc_x *= scale
 lcc_y *= scale
 
