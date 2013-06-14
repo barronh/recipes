193c854163c5dc6bec66398df56d09fadc7ae1d0
barronh <barronh@gmail.com>
2013-06-14 16:23:12 -0300

    Adding x, y, layer, and level if necessary.

--- AddLatLonToNetCDFCFLambertConformal.py
+++ AddLatLonToNetCDFCFLambertConformal.py
@@ -42,25 +42,39 @@ if 'CF' in options.conventions:
     x, y = ifileo.variables['x'][:], ifileo.variables['y'][:]
     lunit = ifileo.variables['x'].units.strip()
 elif 'IOAPI' in options.conventions:
-    lccdef = object()
-    lccdef.latitude_projection_origin = ifileo.YCENT
+    class temp(object): pass
+    
+    lccdef = temp()
+    lccdef.latitude_of_projection_origin = ifileo.YCENT
     lccdef.standard_parallel = [ifileo.P_ALP, ifileo.P_BET]
     lccdef.earth_radius = 6371000. # Add search for earth radius later
     lccdef.longitude_of_central_meridian = ifileo.XCENT
-    x = np.linspace(0, ifileo.NCOLS * ifileo.XCELL, ifileo.NCOLS + 1) + ifileo.XORIG
-    y = np.linspace(0, ifileo.NROWS * ifileo.XCELL, ifileo.NROWS + 1) + ifileo.YORIG
+    x = np.arange(0, ifileo.NCOLS * ifileo.XCELL, ifileo.XCELL) + ifileo.XORIG + ifileo.XCELL / 2.
+    y = np.arange(0, ifileo.NROWS * ifileo.YCELL, ifileo.YCELL) + ifileo.YORIG + ifileo.YCELL / 2.
     lunit = 'm'
 
 lcc = pyproj.Proj('+proj=lcc +lon_0=%s +lat_1=%s +lat_2=%s +a=%s +lat_0=%s' % (lccdef.longitude_of_central_meridian, lccdef.standard_parallel[0], lccdef.standard_parallel[1], lccdef.earth_radius, lccdef.latitude_of_projection_origin,)  )
 
-if 'IOAPI' in options.conventions:
-    x = arrange
 scale = {'km': 1000., 'm': 1.}[lunit]
 lcc_x, lcc_y = np.meshgrid(x, y)
 lcc_x *= scale
 lcc_y *= scale
 
 lon, lat = lcc(lcc_x, lcc_y, inverse = True)
+if 'x' not in ifileo.variables.keys():
+    var = ifileo.createVariable('x', x.dtype.char, ('COL',))
+    var[:] = x[:]
+    var.units = 'km'
+    var._CoordinateAxisType = "GeoX" ;
+
+
+if 'y' not in ifileo.variables.keys():
+    var = ifileo.createVariable('y', x.dtype.char, ('ROW',))
+    var[:] = y[:]
+    var.units = 'km'
+    var._CoordinateAxisType = "GeoY" ;
+
+
 if 'latitude' not in ifileo.variables.keys():
     var = ifileo.createVariable('latitude', lat.dtype.char, ('ROW', 'COL'))
     var[:] = lat
@@ -72,6 +86,19 @@ if 'longitude' not in ifileo.variables.keys():
     var[:] = lon
     var.units = 'degrees_north'
     var.standard_name = 'longitude';
+
+if 'layer' not in ifileo.variables.keys():
+    var = ifileo.createVariable('layer', lon.dtype.char, ('LAY'))
+    var[:] = np.arange(len(ifileo.dimensions['LAY']))
+    var.units = 'model layers'
+    var.standard_name = 'layer';
+    var._CoordinateAxisType = "GeoZ" ;
+
+if 'level' not in ifileo.variables.keys():
+    var = ifileo.createVariable('level', lon.dtype.char, ('LAY'))
+    var[:] = np.arange(len(ifileo.dimensions['LAY']))
+    var.units = 'model level'
+    var.standard_name = 'level';
 
 for varkey in ifileo.variables.keys():
     var = ifileo.variables[varkey]
